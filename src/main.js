fbba0730c885c75ca095b558ba8891b932dc4e3e
Martin Nacimiento <martinnacimiento98@gmail.com>
2020-05-07 00:08:13 -0300

    deploy

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import VueRouter from "vue-router";
 import vuetify from "./plugins/vuetify";
 import router from './router'
+import './registerServiceWorker'
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
