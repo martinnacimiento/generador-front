37f88c6024343eaf845399c5e6c2017a0c3cb29e
Martin Nacimiento <martinnacimiento98@gmail.com>
2020-05-07 01:35:51 -0300

    deploy

--- src/views/CongruencialMulti.vue
+++ src/views/CongruencialMulti.vue
@@ -5,54 +5,103 @@
                 v-card(raised)
                     v-card-title Metodo del congruencial Multiplicativo
                     v-card-text
-                        v-form
+                        v-form(
+                          ref="form"
+                          v-model="valid"
+                          lazy-validation
+                        )
                             v-row
                                 v-col()
                                     v-text-field(
                                         label="Semilla"
                                         v-model="seed"
+                                        :rules="seedRules"
+                                        validate-on-blur
                                     )
                                     v-text-field(
                                         label="Multiplicador A"
                                         v-model="a"
+                                        :rules="aRules"
+                                        validate-on-blur
                                     )
                                 v-col()
                                     v-text-field(
                                         label="modulo M"
                                         v-model="m"
+                                        :rules="mRules"
+                                        validate-on-blur
                                     )
                                     v-text-field(
-                                        label="Cantidad de numeros a generar"
+                                        label="Cantidad de números a generar"
                                         v-model="n"
+                                        :rules="nRules"
+                                        validate-on-blur
                                     )
                     v-card-actions
                         v-spacer
-                        v-btn( @click="generate" color="primary") Generar
+                        v-btn( @click="validate" color="primary") Generar
             v-col
                 v-card(raised)
                     v-data-table(
                         :headers="headers"
                         :items="numbers"
                         :items-per-page="5"
+                        :loading="loading"
                     )
 </template>
 <script>
 import axios from "@/api";
 export default {
   name: "CongruencialMultiplicativo",
-  data: () => ({
-    seed: null,
-    a: null,
-    m: null,
-    n: null,
-    numbers: [],
-    headers: [
-      { text: "Indice", value: "index" },
-      { text: "Numero", value: "number" },
-    ],
-  }),
+  data() {
+    return {
+      seed: null,
+      a: null,
+      m: null,
+      n: null,
+      loading: false,
+      numbers: [],
+      headers: [
+        { text: "Indice", value: "index" },
+        { text: "Numero", value: "number" },
+      ],
+      valid: true,
+
+      seedRules: [
+        (v) => !!v || "La semilla es requerida",
+        (v) =>
+          Number.isInteger(parseInt(v)) ||
+          "La semilla debe ser un numero entero",
+        (v) => this.isPrime(v) || "La semilla debe ser primo",
+        (v) =>
+          !this.isDivisibility(v) ||
+          "La semilla no debe ser divisible por 2 y 5",
+        (v) => v > 0 || "La semilla debe ser mayor que 0",
+      ],
+      aRules: [
+        (v) => !!v || "A es requerida",
+        (v) => Number.isInteger(parseInt(v)) || "A debe ser un numero entero",
+        (v) => this.isPrime(v) || "A debe ser primo",
+        (v) => v > 0 || "A debe ser mayor que 0",
+      ],
+      mRules: [
+        (v) => !!v || "M es requerida",
+        (v) => Number.isInteger(parseInt(v)) || "M debe ser un numero entero",
+        (v) => this.isPrime(v) || "M debe ser primo",
+        (v) => v > 0 || "M debe ser mayor que 0",
+      ],
+      nRules: [
+        (v) => !!v || "La cantidad es requerida",
+        (v) =>
+          Number.isInteger(parseInt(v)) ||
+          "La cantidad debe ser un numero entero",
+        (v) => v > 0 || "La cantidad debe ser mayor que 0",
+      ],
+    };
+  },
   methods: {
     async generate() {
+      this.loading = true;
       let { data } = await axios.post("congruencialMultiplicativo", {
         seed: this.seed,
         m: this.m,
@@ -61,6 +110,32 @@ export default {
       });
       data = data.map((n, i) => ({ index: i + 1, number: n }));
       this.numbers = data;
+      this.loading = false;
+    },
+    isPrime(number) {
+      if (number < 1) return;
+      for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    },
+    /** Verifica la divisibilidad entre 2 y 5 */
+    isDivisibility(seed) {
+      if (seed % 2 == 0 || seed % 5 == 0) {
+        return true;
+      }
+      return false;
+    },
+    validateSeed(seed) {
+      this.isPrime(seed);
+      this.isDivisibility(seed);
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.generate();
+      }
     },
   },
 };
--- src/views/Fibonacci.vue
+++ src/views/Fibonacci.vue
@@ -5,71 +5,125 @@
                 v-card(raised)
                     v-card-title Metodo de Fibonacci
                     v-card-text
-                        v-form
+                        v-form(
+                          ref="form"
+                          v-model="valid"
+                          lazy-validation
+                        )
                             v-row
                                 v-col()
                                     v-text-field(
                                         label="Semilla V1"
-                                        @change="isDivisibility"
+                                        :rules="seedRules"
                                         v-model="seed1"
+                                        validate-on-blur
                                     )
                                     v-text-field(
                                         label="Multiplicador A"
+                                        @change="isPrime"
+                                        :rules="aRules"
                                         v-model="a"
+                                        validate-on-blur
                                     )
                                 v-col()
                                     v-text-field(
                                         label="Semilla V2"
-                                        @change="isDivisibility"
                                         v-model="seed2"
+                                        :rules="seedRules"
+                                        validate-on-blur
                                     )
                                     v-text-field(
                                         label="Cantidad de numeros a generar"
                                         v-model="n"
+                                        :rules="nRules"
+                                        validate-on-blur
                                     )
                     v-card-actions
                         v-spacer
-                        v-btn( @click="generate" color="primary") Generar
+                        v-btn( @click="validate" color="primary") Generar
             v-col
+              v-card(raised)
                 v-data-table(
                     :headers="headers"
                     :items="numbers"
                     :items-per-page="5"
-                    class="elevation-1"
+                    :loading="loading"
                 )
 </template>
 <script>
 import axios from "@/api";
 export default {
   name: "Fibonacci",
-  data: () => ({
-    seed1: null,
-    seed2: null,
-    a: null,
-    n: null,
-    numbers: [],
-    headers: [
-      { text: "Indice", value: "index" },
-      { text: "Numero", value: "number" },
-    ],
-  }),
+  data() {
+    return {
+      seed1: null,
+      seed2: null,
+      a: null,
+      n: null,
+      numbers: [],
+      headers: [
+        { text: "Indice", value: "index" },
+        { text: "Numero", value: "number" },
+      ],
+      valid: true,
+      loading: false,
+
+      //rules
+      seedRules: [
+        (v) => !!v || "La semilla es requerida",
+        (v) =>
+          Number.isInteger(parseInt(v)) ||
+          "La semilla debe ser un numero entero",
+        (v) => this.isPrime(v) || "La semilla debe ser primo",
+        (v) => v > 0 || "La semilla debe ser mayor que 0",
+      ],
+      aRules: [
+        (v) => !!v || "A es requerida",
+        (v) => Number.isInteger(parseInt(v)) || "A debe ser un numero entero",
+        (v) => this.isPrime(v) || "A debe ser primo",
+        (v) => v > 0 || "A debe ser mayor que 0",
+      ],
+      nRules: [
+        (v) => !!v || "La cantidad es requerida",
+        (v) =>
+          Number.isInteger(parseInt(v)) ||
+          "La cantidad debe ser un numero entero",
+        (v) => v > 0 || "La cantidad debe ser mayor que 0",
+      ],
+    };
+  },
   methods: {
     async generate() {
+      this.loading = true;
       let { data } = await axios.post("fibonacci", {
         v1: this.seed1,
         v2: this.seed2,
         a: this.a,
         n: this.n,
       });
-      console.log(data);
-
       data = data.map((n, i) => ({ index: i + 1, number: n }));
       this.numbers = data;
+      this.loading = false;
     },
-    /** Verifica la divisibilidad entre 2 y 5 */
-    isDivisibility(seed) {
-      if (seed && (seed % 2 == 0 || seed % 5 == 0)) {
-        alert("Las semillas no deben ser por 2 y 5");
+    isPrime(number) {
+      if (number < 1) return;
+      for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        if (
+          parseInt(this.a) > parseInt(this.seed1) &&
+          parseInt(this.a) > parseInt(this.seed2)
+        ) {
+          this.generate();
+        } else {
+          alert("A tiene que ser mayor que las semillas.");
+        }
       }
     },
   },
